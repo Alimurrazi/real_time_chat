8f962a80a2d5b04b912b8e0b0442f2b765475e3f
Alimurrazi <alimurrazi570@gmail.com>
2020-08-10 10:48:36 +0600

    connectionMapping add

---
+++ server/server/Domain/Storages/IConnectionMapping.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace server.Domain.Storages
+{
+    public interface IConnectionMapping<T>
+    {
+        public void Add(T key, string connectionId);
+        public void Remove(T key, string connectionId);
+        public IEnumerable<string> GetConnections(T key);
+    }
+}
